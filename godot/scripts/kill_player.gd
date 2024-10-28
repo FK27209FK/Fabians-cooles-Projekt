00e62d266687116490a37d02a418fbe6497a98a5
FK27209FK <bvbfabi5@outlook.de>
2024-10-28 09:23:23 +0100

    audio

--- godot/scripts/kill_player.gd
+++ godot/scripts/kill_player.gd
@@ -1,8 +1,5 @@
 extends Area3D
 
 func _on_body_entered(_body: Node3D) -> void:
-<<<<<<< HEAD
 	$"../../../Player".game_over()
-=======
 	#test
->>>>>>> 7c02d24ebff472852ef473267dc54fd8b60ad792
---
+++ godot/scripts/start_test.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+func _ready() -> void:
+	audio_stream_player_3d.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
