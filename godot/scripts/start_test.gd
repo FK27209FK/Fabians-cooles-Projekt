dade9944ab737bc8571f522f999bef1ecc3204b3
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-11-02 13:52:22 +0100

    Fix audio

--- godot/scripts/start_test.gd
+++ godot/scripts/start_test.gd
@@ -1,11 +1,12 @@
 extends Node
 
-@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
+#@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func _ready() -> void:
-	audio_stream_player_3d.play()
+	#audio_stream_player_3d.play()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
