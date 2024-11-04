f183e7bcddc16901f7b546609fd8202541042f71
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-11-04 16:41:41 +0100

    fix light

--- godot/scripts/stromkasten.gd
+++ godot/scripts/stromkasten.gd
@@ -1,5 +1,11 @@
 extends StaticBody3D
 
+func _ready() -> void:
+	$"../DirectionalLight3D".light_energy = 0.5
+	$StromkastenUngeloest.show()
+	$ungeloest_collisionshape.show()
+	$StromkastenGeloest.hide()
+	$StromkastenGeloest_collision_shape.hide()
 
 func interact():
 	if $StromkastenUngeloest.visible == true:
@@ -9,6 +15,9 @@ func interact():
 		$StromkastenGeloest.show()
 		$StromkastenGeloest_collision_shape.show()
 		$".".set_collision_layer_value(3,false	)
+		while $"../DirectionalLight3D".light_energy < 2:
+			await get_tree().create_timer(0.05).timeout #wait 0.05 Seconds
+			$"../DirectionalLight3D".light_energy = $"../DirectionalLight3D".light_energy + 0.1
 	else:
 		pass
 		
