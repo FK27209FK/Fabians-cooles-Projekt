ebe9964ace3418d29d8cb278fd0509f4a799cf01
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-11-07 16:40:28 +0100

    tuer1

--- godot/assets/Tür_oefnen_schalter.gd
+++ godot/assets/Tür_oefnen_schalter.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 @export var showLabel = true
+@export var switchActive = false
 
 func interact():
-	if showLabel:
+	if showLabel && !switchActive:
 		$"..".rotate_x(20)
-		showLabel = !showLabel
+		switchActive = !switchActive
+		$"../../../Räume/Raum 1/tuer1/AnimationPlayer".play("tuerOeffnen")
+	elif showLabel && switchActive:
+		$"..".rotate_x(-20)
+		switchActive = !switchActive
+		$"../../../Räume/Raum 1/tuer1/AnimationPlayer".play_backwards("tuerOeffnen")
