a8a309345b62bb33c412e965b6024da1b1a94058
BugsSindFeatures <147187131+BugsSindFeatures@users.noreply.github.com>
2024-11-11 09:24:32 +0100

    gegner 2

--- godot/assets/gegner_bewegung.gd
+++ godot/assets/gegner_bewegung.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var gegner: Node3D = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +8,11 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	var move = true
+	var movespeed = 1
+	var x_move = 1
+	var z_move = 1
+	if (move):
+		gegner.position = gegner.position +  (Vector3(x_move,0,z_move) * delta * movespeed)
+		
