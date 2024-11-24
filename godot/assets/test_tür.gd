9032fe1fcf21fe97f9578b38aacb7a9e1b8e746d
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-11-24 21:40:10 +0100

    Recreated second and third room with functioning doors

--- godot/assets/test_tür.gd
+++ godot/assets/test_tür.gd
@@ -1,13 +1,13 @@
-extends StaticBody3D
+extends CSGBox3D
 
 var showLabel = true
 
 func interact():
-	
-	if ($"../../../../Player/Camera/SchlüsselPoint".get_child_count() >= 1):
+
+	if ($"../../../Player/Camera/SchlüsselPoint".get_child_count() >= 1):
 		print("Test Tür geöffnet!")
 		
-		get_tree().queue_delete($"../../../../Player/Camera/SchlüsselPoint".get_child(0))
+		get_tree().queue_delete($"../../../Player/Camera/SchlüsselPoint".get_child(0))
 		get_tree().queue_delete(get_parent())
 	else: 
 		print("Du hast den Schlüssel nicht gefunden!")
