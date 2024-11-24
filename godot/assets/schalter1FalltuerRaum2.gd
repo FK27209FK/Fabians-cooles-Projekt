c86bef0ba304fc365c1e15fdf544edefc0c64a8d
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-11-24 22:39:26 +0100

    Repositioning of Objects + Falltür logic structure

---
+++ godot/assets/schalter1FalltuerRaum2.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var showLabel = true
+@export var switchActive = false
+@onready var alarmanlage: Node3D = $"../../../../../Alarmanlage"
+
+func interact():
+	
+	if showLabel && !switchActive && !alarmanlage.isAktiv():
+		$"..".rotate_x(20)
+		switchActive = !switchActive
+		#TODO: Animation für Falltür abspielen
+	elif showLabel && switchActive:
+		$"..".rotate_x(-20)
+		switchActive = !switchActive
+		#TODO: Animation für Falltür rückwärts abspielen
