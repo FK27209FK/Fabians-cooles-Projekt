196596df8afec0e997b2b594e0d8d692bd6fb18f
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-11-26 19:14:46 +0100

    solves #35

--- godot/assets/schalter1FalltuerRaum2.gd
+++ godot/assets/schalter1FalltuerRaum2.gd
@@ -9,8 +9,10 @@ func interact():
 	if showLabel && !switchActive && !alarmanlage.alarmanlageAktiv:
 		$"..".rotate_x(20)
 		switchActive = !switchActive
-		#TODO: Animation für Falltür abspielen
+		#Animation für Falltür abspielen
+		$"../../../Falltuer1/AnimationPlayer".play("falltuerOeffnen")
 	elif showLabel && switchActive:
 		$"..".rotate_x(-20)
 		switchActive = !switchActive
-		#TODO: Animation für Falltür rückwärts abspielen
+		#Animation für Falltür rückwärts abspielen
+		$"../../../Falltuer1/AnimationPlayer".play_backwards("falltuerOeffnen")
