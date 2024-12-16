7cb6c5734934b9d8aef753f7926e401e4aad6303
BugsSindFeatures <147187131+BugsSindFeatures@users.noreply.github.com>
2024-12-16 09:16:59 +0100

    solves #30

--- godot/scripts/interact.gd
+++ godot/scripts/interact.gd
@@ -8,8 +8,13 @@ func _process(_delta: float) -> void:
 	# Eigentlich ist da 1 und 3 an
 	# - .collision_layer ist 5, da 5 ist binär: 101 , also erster und Dritter Bit sind true restlichen false weil default 0 
 	if is_colliding() && get_collider().collision_layer == 5 && get_collider().showLabel:
-		#print("show label")
 		$"../..".vis_true()
+		var interactText
+		var collider = get_collider()
+		$InteractControl/Label.text = "Interact [E]"
+		if "interactText" in collider:
+			var text = collider.interactText
+			$InteractControl/Label.text = text
 		if Input.is_action_just_pressed("interact"):
 			get_collider().call("interact")
 			$"../..".vis_false()
