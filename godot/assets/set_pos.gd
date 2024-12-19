84c24d1f0d31307a6a34f943277c6bc021980deb
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-12-19 20:41:43 +0100

    solves #52

--- godot/assets/set_pos.gd
+++ godot/assets/set_pos.gd
@@ -1,11 +1,5 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	position = Vector3(3.7, 1.1, -3)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_aufzug_body_entered(body: Node3D) -> void:
+	if $"../../Schalter 1/Schalter_Hebel/StaticBody3D".switchActive:
+		$AnimationPlayer2.play("aufzug_2")
