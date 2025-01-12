765c6997abb8cb986f157ed8d590cee5e57b808b
BugsSindFeatures <Julian.Kausch@outlook.de>
2025-01-12 13:01:03 +0100

    background and light improvements Hintergrundbild lizenzfrei (Pixabay)

--- godot/scripts/sicherungskasten.gd
+++ godot/scripts/sicherungskasten.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var collider
 @onready var lastpath
 @onready var solved: int = 0
-@onready var lights = [$"../LampeRaum1/DirectionalLight3D"]
+@onready var lights = [$"../SpotLight3D"]
 @export var interactText = "Verbinde alle Kabel [e]"
 
 func interact(collider):
@@ -11,8 +11,8 @@ func interact(collider):
 	path = str(path)
 	path = path.replace("/StaticBody3D", "")
 	if path != "Kasten" && (path.ends_with("O") or path.ends_with("U")):
-		if (path.replace("_O","_U") == lastpath) or (path.replace("_U","_O") == lastpath):
-			print("match")
+		if ((path.replace("_O","_U") == lastpath) or (path.replace("_U","_O") == lastpath)) and path != lastpath:
+			print("Kabel verbunden")
 			var mitte = path
 			mitte = mitte.replace("_O","_geloest")
 			mitte = mitte.replace("_U","_geloest")
@@ -21,10 +21,12 @@ func interact(collider):
 			node.visible = true
 			solved = solved + 1
 			if solved == 6:
+				print("light")
 				showLabel = false
 				for light in lights:
+					light.light_color = Color(1,1,1) #Farbe zu weiß ändern
 					while light.light_energy < 2:
-						await get_tree().create_timer(0.05).timeout #wait 0.05 Seconds
+						await get_tree().create_timer(0.05).timeout #Warte 0.05 Sekunden
 						light.light_energy = light.light_energy + 0.1
 		else:
 			lastpath = path	
