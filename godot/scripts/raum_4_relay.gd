bf4bb54ca7b47ad4af5d52880c2a030824f3d90e
Julian <147187131+BugsSindFeatures@users.noreply.github.com>
2025-02-17 09:01:59 +0100

    gang added

--- godot/scripts/raum_4_relay.gd
+++ godot/scripts/raum_4_relay.gd
@@ -1,5 +1,5 @@
 extends Node3D
-var lights = [$SpotLight3D,$SpotLight3D2,$SpotLight3D3,$SpotLight3D4,$SpotLight3D5,$SpotLight3D6,$SpotLight3D7,$SpotLight3D8,$SpotLight3D9,$SpotLight3D10,$SpotLight3D11]
+var lights = [%SpotLight3D,%SpotLight3D2,%SpotLight3D3,%SpotLight3D4,%SpotLight3D5,%SpotLight3D6,%SpotLight3D7,%SpotLight3D8,%SpotLight3D9,%SpotLight3D10,%SpotLight3D11]
 
 func _on_sicherungskasten_2_licht_an() -> void:
 	for light in lights:
