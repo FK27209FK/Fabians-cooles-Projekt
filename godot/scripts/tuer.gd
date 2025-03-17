bcec7f26f69257ba9b71a92745de3ee1c27f403e
noah_schule <test2@kochen-mit-chatgpt.de>
2025-03-17 08:42:34 +0100

    Tisch_Eckig added

--- godot/scripts/tuer.gd
+++ godot/scripts/tuer.gd
@@ -1,12 +1,12 @@
 extends StaticBody3D
 
 @onready var showLabel = true
-@onready var interactText = "Tuer oeffnen [e]"
+@onready var interactText = "Tür öffnen [e]"
 
 func interact():
-	if interactText == "Tuer oeffnen [e]":
+	if interactText == "Tür öffnen [e]":
 		$"../../AnimationPlayer".play("oeffnen")
-		interactText = "Tuer schliessen [e]"
+		interactText = "Tür schliessen [e]"
 	else:
 		$"../../AnimationPlayer".play_backwards("oeffnen")
-		interactText = "Tuer oeffnen [e]"
+		interactText = "Tür öffnen [e]"
