658d0da94350287e98e6eb2c5d6d3b8a4fcc856e
Julian <147187131+BugsSindFeatures@users.noreply.github.com>
2025-04-28 09:21:27 +0200

    Improvements
    - Added Bed colliders
    - some more

--- godot/scripts/use_pc.gd
+++ godot/scripts/use_pc.gd
@@ -2,22 +2,26 @@ extends StaticBody3D
 
 @export var showLabel = true
 @export var interactText = "PC Starten [E]"
+@export var player: Player
+@export var PC: Control
+@export var UI: Control
+@export var Mittelpunkt: ColorRect
 
 func interact():
-	if %Player.playerIsAlive && showLabel:
+	if player.playerIsAlive && showLabel:
 		showLabel = !showLabel
-		#Computer-UI (#23)
-		%PC.visible = true
-		%Player/UI.visible = false
-		%Player.release_mouse()
-		%Player/Camera/Interact/ColorRect.visible = false
+		PC.visible = true
+		UI.visible = false
+		player.release_mouse()
+		Mittelpunkt.visible = false
+		
 	else:
 		showLabel = false
 		print("Player is not allowed to do this action!")
 
 func _on_pc_relay() -> void:
-	%PC.visible = false
-	%Player/UI.visible = true
-	%Player.capture_mouse()
+	PC.visible = false
+	UI.visible = true
+	player.capture_mouse()
 	showLabel = true
-	%Player/Camera/Interact/ColorRect.visible = true
+	Mittelpunkt.visible = true
