609b1c4087b819d3bbbfcc3ddc5b1b7bed3d49cb
noah_schule <test2@kochen-mit-chatgpt.de>
2025-05-12 08:38:19 +0200

    wip

--- godot/assets/Instances/pc.gd
+++ godot/assets/Instances/pc.gd
@@ -8,3 +8,7 @@ func _on_exit_pressed() -> void:
 
 func _on_reaktortimer_timer_aktualisieren(time) -> void:
 	%Reaktortimer.text = time
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
